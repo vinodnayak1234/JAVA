d1b81115d9f1c7c8a150c3da98831154e406bcb9
vinodnayak1234 <106953673+vinodnayak1234@users.noreply.github.com>
2024-06-12 09:12:33 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,17 @@
+package ChatGpt;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i ++) {
+            System.out.println(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
